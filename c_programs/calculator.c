d9fbe5100796962142063eb7416a4f2651a11f8c
Avanthika-Raj-m <avanthikarajm@gmail.com>
2026-03-11 22:46:06 +0530

    defined division function

--- c_programs/calculator.c
+++ c_programs/calculator.c
@@ -18,4 +18,10 @@ float subtract(float a,float b){
 float multiply(float a,float b){
     return a*b;
 }
-
+float divide(float a,float b){
+    if (b==0){
+    printf("Division by zero is not possible");
+    return 0;
+    }
+    return a/b;
+}
