71d5077a73bc77b2b9fc7816a62f77eb038cd30d
Avanthika-Raj-m <avanthikarajm@gmail.com>
2026-03-11 22:37:34 +0530

    declared 6 calculator functions

--- c_programs/calculator.c
+++ c_programs/calculator.c
@@ -1,2 +1,9 @@
 #include<stdio.h>
 #include<math.h>
+float add(float a,float b);
+float subtract(float a,float b);
+float multiply(float a,float b);
+float divide(float a,float b);
+float power(float base,float power);
+float squareroot(int num);
+
